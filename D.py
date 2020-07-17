7660a3433ee4b757b3f243a7c548894b47d8679c
Vishal Rashinkar <68375738+Vishalpr7057@users.noreply.github.com>
2020-07-17 14:52:26 +0530

    Add files via upload

---
+++ D.py
@@ -0,0 +1,5 @@
+
+a=int(input("Enter first number:" ))
+b=int(input("Enter second number:" ))
+print("Integer division of the two numbers is:",a//b)
+print("Float division of the two numbers is:",a/b)
